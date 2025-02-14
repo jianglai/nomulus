467d33c08a505b8d215a08937f4d121ae5be3edf
Lai Jiang <jianglai@google.com>
2025-02-13 22:56:37 -0500

    WIP

--- jetty/deploy-nomulus-for-env.sh
+++ jetty/deploy-nomulus-for-env.sh
@@ -37,16 +37,10 @@ for service in frontend backend pubapi console
 do
   sed s/GCP_PROJECT/"${project}"/g "./kubernetes/nomulus-${service}.yaml" | \
   sed s/ENVIRONMENT/"${environment}"/g | \
-  sed s/PROXY_ENV/"${environment}"/g | \
-  sed s/EPP/"epp"/g | \
-  sed s/WHOIS/"whois"/g | \
   kubectl apply -f -
   # canary
   sed s/GCP_PROJECT/"${project}"/g "./kubernetes/nomulus-${service}.yaml" | \
   sed s/ENVIRONMENT/"${environment}"/g | \
-  sed s/PROXY_ENV/"${environment}_canary"/g | \
-  sed s/EPP/"epp-canary"/g | \
-  sed s/WHOIS/"whois-canary"/g | \
   sed s/"${service}"/"${service}-canary"/g | \
   kubectl apply -f -
 done
